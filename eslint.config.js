3b0ed53287cd8a85dd964b425b76884e72215999
Adam Andrysiak <aadam606@gmail.com>
2025-02-27 15:45:49 +0100

    feat: prettier, eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -3,23 +3,60 @@ import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
+import prettier from 'eslint-plugin-prettier';
 
-export default tseslint.config(
-  { ignores: ['dist', 'node_modules', 'build'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+export default tseslint.config({
+  ignores: ['dist', 'node_modules', 'build'],
+  // extends: [js.configs.recommended, ...tseslint.configs.recommended],
+  files: ['**/*.{ts,tsx}'],
+  languageOptions: {
+    ecmaVersion: 2022,
+    globals: globals.browser,
+    parser: tseslint.parser,
+    parserOptions: {
+      sourceType: 'module',
+      ecmaFeatures: {
+        jsx: true,
+      },
     },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-    },
-  }
-);
+  },
+  plugins: {
+    'react-hooks': reactHooks,
+    'react-refresh': reactRefresh,
+    'simple-import-sort': simpleImportSort,
+    'jsx-a11y': jsxA11y,
+    prettier,
+  },
+  rules: {
+    ...js.configs.recommended.rules,
+    ...tseslint.configs.recommended.rules,
+    ...reactHooks.configs.recommended.rules,
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    indent: ['error', 2],
+    'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    'simple-import-sort/imports': [
+      'error',
+      {
+        groups: [
+          ['^react', '^@?\\w'],
+          ['^(@|components)(/.*|$)'],
+          ['^\\u0000'],
+          ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+          ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+          ['^.+\\.?(css)$'],
+        ],
+      },
+    ],
+    'jsx-a11y/alt-text': 'warn',
+    'jsx-a11y/anchor-is-valid': 'warn',
+    'prettier/prettier': ['error', { singleQuote: true, semi: true }],
+    'no-console': 'warn',
+  },
+});
--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+
 import './App.css';
 
 function App() {
@@ -7,9 +8,11 @@ function App() {
   return (
     <>
       <h1>Vite + React</h1>
-      <div className='card'></div>
-      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
-      <h1 className='text-4xl font-bold underline'> Hello world! </h1>
+      <div className="card"></div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      <h1 className="text-4xl font-bold underline"> Hello world! </h1>
     </>
   );
 }
